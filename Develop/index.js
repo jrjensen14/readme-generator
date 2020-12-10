848693b213ab78ebdb87ee71f6db40ef1ea91bf5
JRJ <jjsocc171@msn.com>
2020-12-10 17:19:28 -0600

    questions entered

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,60 @@
 
 const inquirer = require('inquirer');
+const Choices = require('inquirer/lib/objects/choices');
 //console.log(inquirer)
-inquirer
-    .prompt ([
+const promptReadmeQuestions = () => {
+    return inquirer.prompt ([
         {
             type: 'input',
-            name: 'name',
-            message: 'what is your name',
-        }
-    ])
-    .then(answers => console.log(answers));
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter GitHub username.',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email address.',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provice a description of your project?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Steps required to install your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'do you have a screenshot of your project?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'who contributed on your project?',
+        },
+        {
+            type: 'input',
+            name: 'licence',
+            message: 'do you have a licence for your project?',
+            default: false,
+            choices: ['','','','']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the Github link to your project?',
+        },
+    ]);
+};
+promptReadmeQuestions().then(answers => console.log(answers));
 
 // array of questions for user
 const questions = [
